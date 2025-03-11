38cc7ba6374d066bc620b770881f172de4db0609
harounaKane <harouna.Kane@hotmail.fr>
2025-03-11 17:53:33 +0100

    changement avec autoload spl

--- Entity/User.php
+++ Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 
-class User{
+class User
+{
 
     private $id;
     private $prenom;
@@ -12,44 +13,107 @@ class User{
     private $cp;
     private $dateInscription;
 
-    public function __construct( $id,  $prenom,  $nom,  $login,  $mdp,  $role,  $adresse,  $cp,  $dateInscription){$this->id = $id;$this->prenom = $prenom;$this->nom = $nom;$this->login = $login;$this->mdp = $mdp;$this->role = $role;$this->adresse = $adresse;$this->cp = $cp;$this->dateInscription = $dateInscription;}
-	
-    public function getId() {return $this->id;}
+    public function __construct($id,  $prenom,  $nom,  $login,  $mdp,  $role,  $adresse,  $cp,  $dateInscription)
+    {
+        $this->id = $id;
+        $this->prenom = $prenom;
+        $this->nom = $nom;
+        $this->login = $login;
+        $this->mdp = $mdp;
+        $this->role = $role;
+        $this->adresse = $adresse;
+        $this->cp = $cp;
+        $this->dateInscription = $dateInscription;
+    }
 
-	public function getPrenom() {return $this->prenom;}
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	public function getNom() {return $this->nom;}
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
 
-	public function getLogin() {return $this->login;}
+    public function getNom()
+    {
+        return $this->nom;
+    }
 
-	public function getMdp() {return $this->mdp;}
+    public function getLogin()
+    {
+        return $this->login;
+    }
 
-	public function getRole() {return $this->role;}
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
 
-	public function getAdresse() {return $this->adresse;}
+    public function getRole()
+    {
+        return $this->role;
+    }
 
-	public function getCp() {return $this->cp;}
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
 
-	public function getDateInscription() {return $this->dateInscription;}
+    public function getCp()
+    {
+        return $this->cp;
+    }
 
-	
-    public function setId( $id): void {$this->id = $id;}
+    public function getDateInscription()
+    {
+        return $this->dateInscription;
+    }
 
-	public function setPrenom( $prenom): void {$this->prenom = $prenom;}
 
-	public function setNom( $nom): void {$this->nom = $nom;}
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
 
-	public function setLogin( $login): void {$this->login = $login;}
+    public function setPrenom($prenom): void
+    {
+        $this->prenom = $prenom;
+    }
 
-	public function setMdp( $mdp): void {$this->mdp = $mdp;}
+    public function setNom($nom): void
+    {
+        $this->nom = $nom;
+    }
 
-	public function setRole( $role): void {$this->role = $role;}
+    public function setLogin($login): void
+    {
+        $this->login = $login;
+    }
 
-	public function setAdresse( $adresse): void {$this->adresse = $adresse;}
+    public function setMdp($mdp): void
+    {
+        $this->mdp = $mdp;
+    }
 
-	public function setCp( $cp): void {$this->cp = $cp;}
+    public function setRole($role): void
+    {
+        $this->role = $role;
+    }
 
-	public function setDateInscription( $dateInscription): void {$this->dateInscription = $dateInscription;}
+    public function setAdresse($adresse): void
+    {
+        $this->adresse = $adresse;
+    }
 
-	
+    public function setCp($cp): void
+    {
+        $this->cp = $cp;
+    }
+
+    public function setDateInscription($dateInscription): void
+    {
+        $this->dateInscription = $dateInscription;
+    }
 }
--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
 
+spl_autoload_register(function($classe){
+    $page = "Entity/" . $classe . ".php";
+
+    if( file_exists($page) ){
+        include $page;
+    }
+});
